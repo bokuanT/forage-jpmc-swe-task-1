6e292aef5ff1155e7d474259194a017998a9b367
bokuanT <bokuan2000@gmail.com>
2023-06-09 14:39:03 +0800

    Add getDataPoint unit tests

--- client_test.py
+++ client_test.py
@@ -8,6 +8,7 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getDataPoint(quotes[0]), ('ABC', 120.48, 121.2, (120.48 + 121.2) / 2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,7 +16,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    actual = getDataPoint(quotes[1])
+    self.assertEqual(actual, ('DEF', 117.87, 121.68, (121.68 + 117.87) / 2))
 
   """ ------------ Add more unit tests ------------ """
 
